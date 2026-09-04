630f22e64b0cf504984d143f670692e8cfbe688d
micro <edsonerione@gmail.com>
2026-09-04 16:00:10 -0300

    exercício 52 feito

---
+++ 03 - Condições Aninhadas/Ex052-Num_Primo.py
@@ -0,0 +1,18 @@
+a = int(input("Digite um numero inteiro: "))
+cont = 0
+
+for c in range(1, a+1):
+    if a % c == 0:
+        print(f'\033[32m {c}', end= ' ')
+        cont += 1
+
+    else:
+        print(f'\033[31m {c}', end= ' ')
+
+if cont > 2:
+    print(f'\nO número foi contado {cont} vezes e portanto não  é primo!')
+else:
+    print(f'\nO número foi contado {cont} vezes e portanto o número é primo!')
+
+
+# nível de dificuldade mais alto, precisei raciocinar bem e precise de uma leve ajuda
