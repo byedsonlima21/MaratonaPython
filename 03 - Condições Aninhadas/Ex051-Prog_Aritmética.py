022641f3fe70a2307ea22f088ba6a5cc3ee9b2bd
micro <edsonerione@gmail.com>
2026-09-04 15:27:24 -0300

    exercício 51 melhorado!

--- 03 - Condições Aninhadas/Ex051-Prog_Aritmética.py
+++ 03 - Condições Aninhadas/Ex051-Prog_Aritmética.py
@@ -1,7 +1,9 @@
 a1 = int(input("Qual é o primeiro termo da progressão: "))
+n = int(input('Até qual termo você deseja ver? '))
 r = int(input("Qual é a razão? "))
+fim = a1 + (n * r)
 
-for c in range(1, 10):
-    print(f'{a1 + ( c - 1) * r}', end= ' => ')
+for c in range(a1, fim, r):
+    print(f'{c}', end= ' => ')
 
 print("Acabou!")
