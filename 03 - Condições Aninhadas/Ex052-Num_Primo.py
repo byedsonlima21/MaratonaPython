160ba72cd78ce5cea2517f7f51c189c1f0c9dac0
micro <edsonerione@gmail.com>
2026-09-04 16:10:58 -0300

    exercício 52 melhorado, sendo assim, evitando números menores que 1

--- 03 - Condições Aninhadas/Ex052-Num_Primo.py
+++ 03 - Condições Aninhadas/Ex052-Num_Primo.py
@@ -1,13 +1,16 @@
 a = int(input("Digite um numero inteiro: "))
 cont = 0
 
-for c in range(1, a+1):
-    if a % c == 0:
-        print(f'\033[32m {c}', end= ' ')
-        cont += 1
+if a <= 1:
+    print(f'O número {a} não é primo')
+else:
+    for c in range(1, a+1):
+        if a % c == 0:
+            print(f'\033[32m {c}', end= ' ')
+            cont += 1
 
-    else:
-        print(f'\033[31m {c}', end= ' ')
+        else:
+            print(f'\033[31m {c}', end= ' ')
 
 if cont > 2:
     print(f'\nO número foi contado {cont} vezes e portanto não  é primo!')
