e929f553b624e4f2cabb2cac0d394163277037c4
micro <edsonerione@gmail.com>
2026-09-05 14:48:20 -0300

    exercício 54 feito

---
+++ 03 - Condições Aninhadas/Ex054-Grupo_Maioridade.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+hoje = date.today().year
+maior = 0
+menor = 0
+
+for i in range(1, 4):
+    a = int(input(f'Em que ano a pessoa {i}ª nasceu? '))
+    idade = hoje - a
+    if idade > 18:
+        maior += 1
+    else:
+        menor += 1
+
+print(f'\033[31m{menor} pessoas maiores de idade\033[m')
+print(f'\033[32m{maior} pessoas maiores de idade\033[m')
