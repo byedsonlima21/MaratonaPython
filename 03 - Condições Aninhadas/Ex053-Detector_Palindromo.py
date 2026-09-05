a97127b8e52628f5c82e36bc04a0ae097f942372
micro <edsonerione@gmail.com>
2026-09-04 22:06:41 -0300

    exercício 53 feito

---
+++ 03 - Condições Aninhadas/Ex053-Detector_Palindromo.py
@@ -0,0 +1,10 @@
+a = input('Digite algo que queira testar: ').upper().replace(' ', '')
+b = a[::-1]
+
+for i in range(0, len(b)):
+    if b == a:
+        print('\033[32mTemos um palíndromo\033[m')
+        break
+    else:
+        print('\033[31;40mO que você digitou não é um palíndromo\033[m')
+        break
