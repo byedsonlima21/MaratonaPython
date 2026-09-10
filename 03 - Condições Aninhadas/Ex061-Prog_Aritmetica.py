9c39249d8e4ae9fbbdd3752c1e8603dbad0cfec9
micro <edsonerione@gmail.com>
2026-09-10 15:15:08 -0300

    exercício 61 parcialmente feito, depois rever e corrigir

---
+++ 03 - Condições Aninhadas/Ex061-Prog_Aritmetica.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+print("Vamos calcular uma PA!\n")
+sleep(0.4)
+a1 = int(input('Primeiro valor: '))
+a = a1
+r = int(input('Qual a razão? '))
+pg = 10
+res = 0
+cont = 1
+
+while cont <= 10:
+    print(f'{a1}', end=' -> ')
+    res = a1 + r
+    a1 = res
+    cont += 1
+    if cont == 11:
+        print(f'\nACABOU!')
