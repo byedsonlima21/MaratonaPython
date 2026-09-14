f59b2e790b98909c05fa3b39de6a8e853c59f4c2
micro <edsonerione@gmail.com>
2026-09-14 15:10:06 -0300

    Exercício 67

---
+++ 05 - Estruturas de Repetições - While/Ex067-TabuadaV3.0.py
@@ -0,0 +1,8 @@
+while True:
+    num = int(input('Quer ver a tabuada de qual valor? '))
+    for c in range(1, 11):
+        if num >= 0:
+            print(f'{num} x {c:2} = {num * c:2}')
+    else:
+        print(f'Programa finalizado!')
+        break
