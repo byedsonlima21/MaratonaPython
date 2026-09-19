8694cdccf0ffb4cfe494bbf1079d2d3651d276a0
micro <edsonerione@gmail.com>
2026-09-19 11:08:57 -0300

    Exercício 77 feito

--- 06 - Estruturas de Dados - Tuplas/Ex077-Contando_Vogais.py
+++ 06 - Estruturas de Dados - Tuplas/Ex077-Contando_Vogais.py
@@ -1,4 +1,6 @@
-palavra = ('estudar', 'python', 'programação', 'escola', 'programador', 'mercado', 'praticar', 'trabalhar')
+palavra = ('estudar', 'python', 'programação', 'escola', 'programador', 'mercado', 'praticar', 'trabalhar',
+           'hoje', 'azul', 'parte', 'coragem', 'computador', 'carro',
+           'cachorro', 'jabuticaba', 'jericoacoara', 'filtro')
 
 for i in palavra:
     print(f'\nNa palavra {i.upper()} temos ', end='')
