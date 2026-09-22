1ce5a14c011c039b783f2e58ff4b33a40e284bd6
micro <edsonerione@gmail.com>
2026-09-22 11:53:28 -0300

    Exercício 78 feito

--- 07 - Estruturas de Dados - Listas/Ex078-Maior_e_Menor.py
+++ 07 - Estruturas de Dados - Listas/Ex078-Maior_e_Menor.py
@@ -4,5 +4,15 @@ for i in range(5):
     lista.append(int(input(f'Digite um valor para a posição {i + 1}: ')))
 
 print(f'Você digitou os valores {lista}')
-print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista)) + 1}')
-print(f'O menor valor digitado foi {min(lista)} na posição {lista.index(min(lista)) + 1}')
+
+maior = max(lista)
+menor = min(lista)
+
+print(f'O maior valor digitado foi {max(lista)} nas posições: ', end='')
+for i, v in enumerate(lista):
+    if v == maior:
+        print(f'{i + 1}... ', end='')
+print(f'\nO menor valor digitado foi {min(lista)} nas posições: ', end='')
+for i, v in enumerate(lista):
+    if v == menor:
+        print(f'{i + 1}... ', end='')
