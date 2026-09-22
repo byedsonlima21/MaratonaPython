6392d53e88e9d1c249d261d7b3bbd94f000bc224
micro <edsonerione@gmail.com>
2026-09-22 11:13:40 -0300

    Exercício 78 feito

--- 07 - Estruturas de Dados - Listas/Ex078-Maior_e_Menor.py
+++ 07 - Estruturas de Dados - Listas/Ex078-Maior_e_Menor.py
@@ -1,8 +1,7 @@
 lista = []
 
 for i in range(5):
-    num=int(input(f'Digite um valor para a posição {i + 1}: '))
-    lista.append(num)
+    lista.append(int(input(f'Digite um valor para a posição {i + 1}: ')))
 
 print(f'Você digitou os valores {lista}')
 print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista)) + 1}')
