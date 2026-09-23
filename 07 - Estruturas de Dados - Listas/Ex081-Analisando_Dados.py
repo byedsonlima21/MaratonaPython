b523c1cb31d04e783d235c67468ab72f183abc29
micro <edsonerione@gmail.com>
2026-09-23 19:08:26 -0300

    Exercício 81 feito

---
+++ 07 - Estruturas de Dados - Listas/Ex081-Analisando_Dados.py
@@ -0,0 +1,17 @@
+num = []
+
+while True:
+    num.append(int(input('Digite um valor: ')))
+
+    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp in 'Nn':
+        break
+
+print(f'Você digitou {len(num)} elementos.')
+num.sort(reverse=True)
+print(f'A lista em ordem decrescente: {num}')
+
+if 5 in num:
+    print('O valor 5 apareceu na lista.')
+else:
+    print('Na lista não tem o valor 5')
